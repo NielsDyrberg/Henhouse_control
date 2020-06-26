69d20d33f11434ea3eaddbd6e6d83fe4df0132a5
NielsDyrberg <ncpd@live.dk>
2020-06-27 00:27:15 +0200

    Ignore update

---
+++ Receiver/Receiver/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Receiver.cpp
+ *
+ * Created: 27/06/2020 00:21:23
+ * Author : ncpd
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
